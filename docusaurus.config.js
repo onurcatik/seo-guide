181a855539212e5072a5a6eb4a7ce8df1d7c3dd4
onurcatik <catikonur@gmail.com>
2024-12-28 19:56:37 +0300

    workflow added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Docu Website',
+  tagline: 'Bring your website to another level',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
